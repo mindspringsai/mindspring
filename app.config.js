9a50975b38f296f33ce55cee7a5cdb4ae471aa7a
mindspringai <mindspringsai@gmail.com>
2025-08-15 04:31:45 +0100

    app.config.js

--- app.config.js
+++ app.config.js
@@ -1,4 +1,4 @@
-﻿export default {
+export default {
   expo: {
     name: "MindSpring",
     slug: "mindspring",
@@ -7,10 +7,11 @@
       package: "com.ecocat.mindspring",
       permissions: ["RECORD_AUDIO"]
     },
+    // Important for GitHub Pages
     web: {
       bundler: "metro",
-      output: "single",
-      publicPath: "/mindspring"   // <-- important for GitHub Pages subpath
+      output: "single",        // client-only build (no SSR)
+      publicPath: "/mindspring" // site lives under /mindspring
     },
     extra: {
       eas: { projectId: "3b43b80f-77c8-4160-830f-c0897cf8c1af" }
